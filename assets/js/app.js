3996312229c197e4d4643b2272a992237e116572
ksound22 <chrisjay967@gmail.com>
2021-12-14 09:49:23 +0100

    Added email subscription form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,31 @@
+// Hamburger menu selections
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("ul");
+const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
+// scroll-to-top selection
+const scrollUp = document.querySelector("#scroll-up");
 
-function mobileMenu() {
+// Hamburger menu functionality
+hamburger.addEventListener("click", openMenu);
+
+function openMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
-
+// Close menu on nav menu clicks
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
